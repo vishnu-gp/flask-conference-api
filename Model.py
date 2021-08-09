76111376f0cd85533ce7eafd1ac8d3d860c8b331
Vishnu Gp <vishnu.aassk@gmail.com>
2021-08-09 14:42:35 +0530

    Added Talk model with relations

--- Model.py
+++ Model.py
@@ -26,6 +26,24 @@ class Conference(db.Model):
     description = db.Column(db.Text, nullable=True)
     start_date = db.Column(db.Date, nullable=False)
     end_date = db.Column(db.Date, nullable=False)
+    talks = db.relationship('Talk', cascade="all,delete", backref='conferences', lazy=True)
+    creation_date = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
+
+    def __init__(self, title, description, start_date, end_date, talks):
+        self.title = title
+        self.description = description
+        self.start_date = start_date
+        self.end_date = end_date
+        self.talks = talks
+
+class Talk(db.Model):
+    __tablename__ = 'talks'
+    id = db.Column(db.Integer, primary_key=True)
+    conference_id = db.Column(db.Integer, db.ForeignKey('conferences.id'), nullable=False)
+    title = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    duration_min = db.Column(db.Integer, nullable=False)
+    scheduled_at = db.Column(db.DateTime, nullable=False)
     creation_date = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
 
     def __init__(self, title, description, start_date, end_date):
@@ -44,3 +62,10 @@ class ConferenceSchema(ma.Schema):
     title = fields.String(required=True)
     start_date = fields.Date(required=True)
     end_date = fields.Date(required=True)
+
+class TalkSchema(ma.Schema):
+    id = fields.Integer()
+    conference_id = fields.Integer()
+    title = fields.String(required=True)
+    duration_min = fields.Integer(required=True)
+    scheduled_at = fields.Date(required=True)
