768753665e05c2288e7ae796940777755cff007b
Vishnu Gp <vishnu.aassk@gmail.com>
2021-08-09 15:02:16 +0530

    Added many to many relationships with connecting tables

--- Model.py
+++ Model.py
@@ -7,6 +7,17 @@ from flask_sqlalchemy import SQLAlchemy
 ma = Marshmallow()
 db = SQLAlchemy()
 
+# Many to Many Relationship connecting tables
+speakers = db.Table('speakers',
+    db.Column('talk_id', db.Integer, db.ForeignKey('talks.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
+)
+
+participants = db.Table('participants',
+    db.Column('talk_id', db.Integer, db.ForeignKey('talks.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True)
+)
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -44,13 +55,19 @@ class Talk(db.Model):
     description = db.Column(db.Text, nullable=True)
     duration_min = db.Column(db.Integer, nullable=False)
     scheduled_at = db.Column(db.DateTime, nullable=False)
+    speakers = db.relationship('User', cascade="all,delete", secondary=speakers, lazy='subquery',
+        backref=db.backref('talks', lazy=True))
+    participants = db.relationship('User', cascade="all,delete", secondary=participants, lazy='subquery',
+        backref=db.backref('talks', lazy=True))
     creation_date = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
 
-    def __init__(self, title, description, start_date, end_date):
+    def __init__(self, title, description, duration_min, scheduled_at, speakers, participants):
         self.title = title
         self.description = description
-        self.start_date = start_date
-        self.end_date = end_date
+        self.duration_min = duration_min
+        self.scheduled_at = scheduled_at
+        self.speakers = speakers
+        self.participants = participants
 
 class UserSchema(ma.Schema):
     id = fields.Integer()
