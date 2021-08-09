9f8777aead8fb3a91cb5752381f9252c2e7ab841
Vishnu Gp <vishnu.aassk@gmail.com>
2021-08-09 13:42:39 +0530

    Added Conference Model

--- Model.py
+++ Model.py
@@ -19,7 +19,28 @@ class User(db.Model):
         self.username = username
         self.email = email
 
+class Conference(db.Model):
+    __tablename__ = 'conferences'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    creation_date = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp(), nullable=False)
+
+    def __init__(self, title, description, start_date, end_date):
+        self.title = title
+        self.description = description
+        self.start_date = start_date
+        self.end_date = end_date
+
 class UserSchema(ma.Schema):
     id = fields.Integer()
     username = fields.String(required=True)
     email = fields.String(required=True)
+
+class ConferenceSchema(ma.Schema):
+    id = fields.Integer()
+    title = fields.String(required=True)
+    start_date = fields.Date(required=True)
+    end_date = fields.Date(required=True)
