ac9e2362ecb8b413252034eaaedcb8089e74dbfa
Vishnu Gp <vishnu.aassk@gmail.com>
2021-08-10 17:55:48 +0530

    Added automated tests for adding and deleteing speakers and participants

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,19 +7,22 @@ sys.path.append('../conference')
 from run import create_app
 from Model import db
 
-
-
 @pytest.fixture
 def client():
 
     app = create_app("configtest")
     app_context = app.app_context()
     app_context.push()
-    db.create_all()
     with app.test_client() as client:
         yield client
 
-# 1 Testing the welcome page
+# 1 Clean and migrate db        
+def test_clean_db(client):
+    db.session.remove()
+    db.drop_all()
+    db.create_all()
+
+# 2 Testing the welcome page
 def test_welcome_page(client):
     response = client.get('api/')
     assert response.status_code == 200
@@ -27,7 +30,7 @@ def test_welcome_page(client):
 
 # Test for Conference Model
 
-# 2 Testing the conference creation
+# 3 Testing the conference creation
 def test_post_conference(client):
     input_data = {"title":"Scala Conference", "description":"This conference will be conducted at Gurgaon", "start_date": "2021-08-11","end_date": "2021-08-18"}
     response = client.post('api/Conference', json=input_data)
@@ -38,14 +41,15 @@ def test_post_conference(client):
     for key in input_data:
         assert response['data'][key] == input_data[key]
 
-# 3 Testing the conference creation fail
+# 4 Testing the conference creation fail
 def test_post_conference_fail(client):
     input_data = ''
     response = client.post('api/Conference', json=input_data)
     assert response.status_code == 400
-    assert b"error" in response.data
+    response = json.loads(response.data)
+    assert response['status'] == 'error'
 
-# 4 Testing the conference edit
+# 5 Testing the conference edit
 def test_patch_conference(client):
     input_data = {"id": 1, "title":"Python Conference", "description":"This conference will be conducted at Delhi", "start_date": "2021-08-12","end_date": "2021-08-20"}
     response = client.patch('api/Conference', json=input_data)
@@ -55,7 +59,7 @@ def test_patch_conference(client):
     for key in input_data:
         assert response['data'][key] == input_data[key]
 
-# 5 Testing the conference edit fail
+# 6 Testing the conference edit fail
 def test_patch_conference_fail(client):
     input_data = {"id": 100, "title":"Python Conference", "description":"This conference will be conducted at Delhi", "start_date": "2021-08-12","end_date": "2021-08-20"}
     response = client.patch('api/Conference', json=input_data)
@@ -63,7 +67,7 @@ def test_patch_conference_fail(client):
     response = json.loads(response.data)
     assert response['status'] == 'error'
 
-# 6 Testing the conference listing
+# 7 Testing the conference listing
 def test_get_conference(client):
     input_data = {"id": 1, "title":"Python Conference", "description":"This conference will be conducted at Delhi", "start_date": "2021-08-12","end_date": "2021-08-20"}
     response = client.get('api/Conference', json=input_data)
@@ -75,7 +79,7 @@ def test_get_conference(client):
 
 # Tests for Talk Model
 
-# 7 Testing adding a Talk
+# 8 Testing adding a Talk
 def test_post_talk(client):
     input_data = {"title":"Python DataType", "description":"This talk is about Python DataTypes", "scheduled_at":"2021-08-15T16:30:00", "duration_min": 120, "conference_id": 1}
     response = client.post('api/Talk', json=input_data)
@@ -85,7 +89,7 @@ def test_post_talk(client):
     for key in input_data:
         assert response['data'][key] == input_data[key]
 
-# 8 Testing adding a Talk fail
+# 9 Testing adding a Talk fail
 def test_post_talk_fail(client):
     input_data = {"title":"Python DataType", "description":"This talk is about Python DataTypes", "scheduled_at":"2021-08-15T16:30:00", "duration_min": 120, "conference_id": 100}
     response = client.post('api/Talk', json=input_data)
@@ -93,7 +97,7 @@ def test_post_talk_fail(client):
     response = json.loads(response.data)
     assert response['status'] == 'error'
 
-# 9 Testing editing a talk
+# 10 Testing editing a talk
 def test_patch_talk(client):
     input_data = {"title":"Python DataTypes", "description":"This talk is about Python and Python DataTypes", "scheduled_at":"2021-08-15T17:30:00", "duration_min": 150, "conference_id": 1, "id":1}
     response = client.patch('api/Talk', json=input_data)
@@ -103,7 +107,7 @@ def test_patch_talk(client):
     for key in input_data:
         assert response['data'][key] == input_data[key]
 
-# 10 Testing editing a talk fail
+# 11 Testing editing a talk fail
 def test_patch_talk_fail(client):
     input_data = {"title":"Python DataTypes", "description":"This talk is about Python and Python DataTypes", "scheduled_at":"2021-08-15T17:30:00", "duration_min": 150, "conference_id": 1, "id":100}
     response = client.patch('api/Talk', json=input_data)
@@ -111,7 +115,7 @@ def test_patch_talk_fail(client):
     response = json.loads(response.data)
     assert response['status'] == 'error'
 
-# 11 Testing add speaker to a talk
+# 12 Testing add speaker to a talk
 def test_post_talk_speaker(client):
     input_data = { "talk_id":1, "user_type": "speaker", "username": "monica", "email":"monica@test.com"}
     response = client.post('api/Talk/User', json=input_data)
@@ -126,7 +130,7 @@ def test_post_talk_speaker(client):
         else:
             assert response['data']['speakers'][0][key] == input_data[key]
 
-# 12 Testing add speaker to a talk fail
+# 13 Testing add speaker to a talk fail
 def test_post_talk_speaker_fail(client):
     input_data = { "talk_id":100, "user_type": "speaker", "username": "monica", "email":"monica@test.com"}
     response = client.post('api/Talk/User', json=input_data)
@@ -134,7 +138,7 @@ def test_post_talk_speaker_fail(client):
     response = json.loads(response.data)
     assert response['status'] == 'error'
 
-# 13 Testing add participant to a talk
+# 14 Testing add participant to a talk
 def test_post_talk_participant(client):
     input_data = { "talk_id":1, "user_type": "participant", "username": "sam", "email":"sam@test.com"}
     response = client.post('api/Talk/User', json=input_data)
@@ -149,7 +153,7 @@ def test_post_talk_participant(client):
         else:
             assert response['data']['participants'][0][key] == input_data[key]
 
-# 14 Testing add participant to a talk
+# 15 Testing add participant to a talk fail
 def test_post_talk_participant_fail(client):
     input_data = { "talk_id":100, "user_type": "participant", "username": "sam", "email":"sam@test.com"}
     response = client.post('api/Talk/User', json=input_data)
@@ -157,6 +161,74 @@ def test_post_talk_participant_fail(client):
     response = json.loads(response.data)
     assert response['status'] == 'error'
 
-def clear_db(client):
-    db.session.remove()
-    db.drop_all()
+#16 Testing list talks under a conference
+def test_get_talk(client):
+    reference_data ={
+        "conference_id": 1,
+        "description": "This talk is about Python and Python DataTypes",
+        "duration_min": 150,
+        "title": "Python DataTypes",
+        "speakers": [
+            {
+                "username": "monica",
+                "email": "monica@test.com",
+                "id": 1
+            }
+        ],
+        "id": 1,
+        "scheduled_at": "2021-08-15T17:30:00",
+        "participants": [
+            {
+                "username": "sam",
+                "email": "sam@test.com",
+                "id": 2
+            }
+        ]
+    }
+    
+    response = client.get('api/Talk?conference_id=1')
+    assert response.status_code == 200
+    response = json.loads(response.data)
+    assert response['status'] == 'success'
+    for key in reference_data:
+        if key == 'speakers' or key == 'participants':
+            for second_key in reference_data[key][0]:
+                assert response['data'][0][key][0][second_key] == reference_data[key][0][second_key]
+        else:
+            assert response['data'][0][key] == reference_data[key]
+    if len(response['data']) > 1:
+        assert False
+
+# 17 Testing Removing a speaker
+def test_delete_talk_speaker(client):
+    input_data = { "talk_id":1, "user_type": "speaker", "username": "monica", "email":"monica@test.com"}
+    response = client.delete('api/Talk/User', json=input_data)
+    assert response.status_code == 201
+    response = json.loads(response.data)
+    assert response['status'] == 'success'
+    assert len(response['data']['speakers']) == 0
+
+# 18 Testing Removing a speaker fail
+def test_delete_talk_speaker_fail(client):
+    input_data = { "talk_id":1, "user_type": "speaker", "username": "monicax", "email":"monica@test.com"}
+    response = client.delete('api/Talk/User', json=input_data)
+    assert response.status_code == 400
+    response = json.loads(response.data)
+    assert response['status'] == 'error'
+
+# 19 Testing Removing a participant
+def test_delete_talk_participant(client):
+    input_data = { "talk_id":1, "user_type": "participant", "username": "sam", "email":"sam@test.com"}
+    response = client.delete('api/Talk/User', json=input_data)
+    assert response.status_code == 201
+    response = json.loads(response.data)
+    assert response['status'] == 'success'
+    assert len(response['data']['participants']) == 0
+
+# 20 Testing Removing a participant fail
+def test_delete_talk_participant_fail(client):
+    input_data = { "talk_id":1, "user_type": "participant", "username": "sam", "email":"samx@test.com"}
+    response = client.delete('api/Talk/User', json=input_data)
+    assert response.status_code == 400
+    response = json.loads(response.data)
+    assert response['status'] == 'error'
